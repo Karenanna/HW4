3633669c63a27b8bd6c5695396a5fdd027fe7eeb
karen gevorgyan <karen-gev.22@mail.ru>
2022-09-28 00:57:28 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args){
+        int age = 17;
+        System.out.println(age);
+        if (age >=18)
+        System.out.println("Поздравляем Вас с совершеннолетием");
+        if (age < 18);
+        System.out.println("Возраст совершеннолетия еще не наступил, нужно еще подаждать");
+
+
+
 
 
     }
